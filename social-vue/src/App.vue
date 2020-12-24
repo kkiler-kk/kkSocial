e13b8993e493c358f8626c0255993bd6e15b7b00
thy <toholyforyou@outlook.com>
2020-12-25 07:31:02 +0800

    vue add scss,evaIcon

--- social-vue/src/App.vue
+++ social-vue/src/App.vue
@@ -1,32 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+		<Header :links="links"/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+	name: 'App',
+	data: function () {
+		return {
+			links: [
+				{
+					text: 'login',
+					iconData: 'person-add',
+					url: '/login',
+					title: 'login'
+				}
+			]
+		} 
+	},
+	components: {
+		Header
+	}
+}
+</script>
+
 <style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+html, body {
+	height: 100%;
+}
+
+.container {
+	width: 1280px;
+	margin: auto;
+	padding: 0 2rem;
+}
+
 #app {
+	display: flex;
+	flex-direction: column;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #414141;
+	font-size: 1.2rem;
+	height: 100%;
+	background-color: #EEEEEE;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* a.router-link-exact-active */
 </style>
---
+++ social-vue/src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+	<div id="header">
+		<div class="container">
+			<div class="title">
+				TITLE
+			</div>
+			
+			<div class="menu">
+				<div 
+					class="item" 
+					v-for="(link, index) of links" 
+					:title="link.title"
+					:key="'l' + index">
+					<router-link :to="link.url">
+						<i :data-eva="link.iconData"></i>
+					</router-link>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Header',
+	props: {
+		links: Array
+	}
+}
+</script>
+
+<style lang="scss">
+#header {
+	line-height: 3.5rem;
+	font-size: 1.5rem;
+	font-weight: bolder;
+	box-shadow: 0 0 1rem #21212161;
+	background-color: #FFFFFF;
+	z-index: 1;
+	
+	> .container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	
+	.menu {
+		display: flex;
+		cursor: pointer;
+		
+		> .item {
+			display: block;
+			width: 3rem;
+			height: 3rem;
+			text-align: center;
+			
+			a {
+				color: #424242;
+				text-decoration: none;
+				
+				&:hover {
+					color: #2979FF;
+				}
+			}
+			
+			&:hover {
+				color: #2979FF;
+			}
+		}
+	}
+}
+</style>
