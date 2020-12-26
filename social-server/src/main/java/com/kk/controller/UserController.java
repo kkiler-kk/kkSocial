cc14e76295d421fd44adcd615744b470b2bad881
KK <3258261356@qq.com>
2020-12-26 21:55:34 +0800

    Test

--- social-server/src/main/java/com/kk/controller/UserController.java
+++ social-server/src/main/java/com/kk/controller/UserController.java
@@ -5,9 +5,11 @@ import com.kk.bean.User;
 import com.kk.service.UserService;
 import com.kk.util.ErrorCode;
 import com.kk.util.LinkData;
+import com.kk.util.StrUtil;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -19,15 +21,18 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/login.do", method = RequestMethod.POST)
-    public Dto<String> login(@RequestParam("email")String email,@RequestParam("password") String password){
-        if(Objects.equals(email, "") || Objects.equals(password, "") || Objects.equals(email, null) || Objects.equals(password, null)) return new Dto<String>(ErrorCode.ILLEGAL_PARAMETER, "邮箱或者密码为空");
+    @ResponseBody
+    public Dto<String> login(@RequestParam(value = "email")String email,@RequestParam(value = "password") String password){
+        if(StrUtil.isEmpty(email) || StrUtil.isEmpty(password)){
+            return new Dto<String>(ErrorCode.ILLEGAL_PARAMETER, "邮箱或者密码为空");
+        }
         //if(!StrUtil.isValidEmail(email)) throw new SecurityException("email no standard")
         Dto<String> result = userService.login(email, password);
         return result;
     }
     @RequestMapping(value = "/register.do", method = RequestMethod.POST)
     public Dto register(String email, String password, String name, Integer gender, @RequestParam("file") MultipartFile file, Integer code){
-        if(Objects.equals(email, "") || Objects.equals(password, "") || Objects.equals(name, "")){
+        if(StrUtil.isEmpty(email) || StrUtil.isEmpty(password) || StrUtil.isEmpty(name)){
             return new Dto(ErrorCode.ILLEGAL_PARAMETER ,"参数为空");
         }
         String url = LinkData.upload(file, email);
@@ -38,10 +43,12 @@ public class UserController {
     @RequestMapping(value = "/existEmail.do", method = RequestMethod.GET)
     @ResponseBody
     public Dto existEmail(String email){
+        if(StrUtil.isEmpty(email)) return new Dto(ErrorCode.ILLEGAL_PARAMETER, "邮箱为NUll");
         return userService.existEmail(email);
     }
     @RequestMapping(value = "/sendEmail.do", method = RequestMethod.GET)
     public void sendEmail(String email){
+        if(StrUtil.isEmpty(email)) return;
         userService.sendEmail(email);
     }
 }
--- social-server/src/main/java/com/kk/util/StrUtil.java
+++ social-server/src/main/java/com/kk/util/StrUtil.java
@@ -1,5 +1,6 @@
 package com.kk.util;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class StrUtil {
@@ -8,5 +9,8 @@ public class StrUtil {
             return Pattern.matches("^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][A-Za-z0-9]+)*$", email);
         }
         return false;
+    }
+    public static boolean isEmpty(String s){
+        return Objects.equals(s, null) || Objects.equals(s, "");
     }
 }
